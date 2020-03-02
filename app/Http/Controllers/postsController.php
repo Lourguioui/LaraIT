2d7a9bf967baae15761401ddf3d4109a85d60124
Lourguioui <iz_lourguioui@esi.dz>
2020-03-02 07:22:13 +0100

    DB

--- app/Http/Controllers/postsController.php
+++ app/Http/Controllers/postsController.php
@@ -2,11 +2,13 @@
 
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class postsController
 {
     public function show($post){
+        $user = DB::select('select * from users');
         $posts=[
             'my-first-post' => 'this is my first post',
             'my-second-post' => 'this is my second post'
@@ -14,6 +16,7 @@ class postsController
         if (! array_key_exists($post,$posts)){
             abort(404,'Sorry this post does not exist');
         }
+        print_r($user);
         return view('post',[
             'post' => $posts[$post]
         ]);
